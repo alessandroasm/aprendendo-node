a37417a8247810858c00bfbb1d4a4a5cf8d5f5a4
Alessandro Menezes <alessandroasm@gmail.com>
2023-07-15 18:23:19 -0400

    Descrição do projeto

--- aula-05-github/README.md
+++ aula-05-github/README.md
@@ -2,5 +2,15 @@
 
 Exemplo de projeto usando o Github para compartilhar o projeto
 
-Danilo: Teste
-outra coisa aqui
+## Como executar o projeto
+
+- Entre no diretório da aula: `cd aula-05-github`
+- Rodar yarn (instala pacotes, dependências): `yarn`
+- Rodar projeto: `yarn start`
+- Abrir Chrome em `http://localhost:5000/`
+
+## Tarefa
+
+- Melhorar layout do site para um cadastro de pacientes
+- Editar o arquivo views/cadastro.handlebars de forma a mostrar campos (tag input)
+- Muita atenção na tag form, ela precisa fazer com que os dados vão para POST /cadastro
--- aula-05-github/src/servidor.js
+++ aula-05-github/src/servidor.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
 const bodyParser = require("body-parser");
+const luxon = require("luxon");
+const fs = require("fs");
 
 const app = express();
 
@@ -15,6 +17,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/cadastro", (req, res) => {
+  res.render("cadastro");
+});
+
+app.post("/cadastro", (req, res) => {
+  const { nome, idade, peso, sintoma } = req.body;
+  const data = luxon.DateTime.now();
+
+  const paciente = `${nome};${idade};${peso};${sintoma};${data}\n`;
+  fs.appendFileSync("./pacientes.txt", paciente);
+
   res.render("cadastro");
 });
 
